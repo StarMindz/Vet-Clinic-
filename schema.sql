e6343022e80deaaf9260dd07c35ab67f4325c15c
starmindz <64619873+StarMindz@users.noreply.github.com>
2022-12-13 18:20:55 +0100

    Add range to name

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE animals (
     id                 INT,
-    name               VARCHAR,
+    name               VARCHAR(100),
     date_of_birth      DATE,
     escape_attempts    INT,
     neutered           BOOLEAN,
