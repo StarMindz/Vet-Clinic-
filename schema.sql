affac2a02d0362ccb65e1652848e56d941abc123
starmindz <64619873+StarMindz@users.noreply.github.com>
2022-12-13 14:45:22 +0100

    Update

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id                 BIGSERIAL,
+    id                 INT,
     name               VARCHAR,
     date_of_birth      DATE,
     escape_attempts    INT,
