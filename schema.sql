602746e787d5f3dfd35469dd0eff0f2c9a8028ba
starmindz <64619873+StarMindz@users.noreply.github.com>
2022-12-13 14:07:48 +0100

    Make id BigSerial type so it autogenerates

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id                 INT,
+    id                 BIGSERIAL,
     name               VARCHAR,
     date_of_birth      DATE,
     escape_attempts    INT,
